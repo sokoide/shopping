4d1eacc971baf8cc1dc3a96ff00dffdd5db10367
So Koide <sokoide@gmail.cojm>
2024-07-07 12:08:42 +0900

    add: OTLP tracing and metrics

--- csharp/shop/Program.cs
+++ csharp/shop/Program.cs
@@ -1,20 +1,28 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
 using Serilog;
 using Serilog.Sinks.OpenTelemetry;
 
-// Configure Serilog
+const string OTEL_SERVICE_NAME = "Nintenbo-backend";
+const string OTLP_GRPC_ENDPOINT = "http://localhost:4317/";
+const string OTLP_HTTP_ENDPOINT = "http://localhost:4318/";
+
+// Configure Serilog & OTLP logging
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .WriteTo.OpenTelemetry(options =>
     {
-        options.Endpoint = "http://localhost:4318/v1/logs";
+        options.Endpoint = OTLP_HTTP_ENDPOINT + "logs";
         options.Protocol = OtlpProtocol.HttpProtobuf;
         options.ResourceAttributes = new Dictionary<string, object>
         {
-            ["service.name"] = "Nintenbo-backend"
+            ["service.name"] = OTEL_SERVICE_NAME,
         };
     })
     // .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
@@ -37,6 +45,36 @@ builder.Services.AddCors(options =>
             .WithOrigins("http://localhost:3000") // Specify the allowed origin
             .AllowAnyHeader()
             .AllowAnyMethod());
+});
+
+// Configure OTLP Tracing and Metrics
+var tracingOtlpEndpoint = OTLP_GRPC_ENDPOINT;
+var otel = builder.Services.AddOpenTelemetry();
+
+// Configure OpenTelemetry Resources with the application name
+otel.ConfigureResource(resource => resource
+    .AddService(serviceName: builder.Environment.ApplicationName));
+
+// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+otel.WithMetrics(metrics => metrics
+    // Metrics provider from OpenTelemetry
+    .AddAspNetCoreInstrumentation()
+    // .AddMeter(greeterMeter.Name)
+    // Metrics provides by ASP.NET Core in .NET 8
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel"));
+// .AddPrometheusExporter());
+
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+    tracing.AddSource(OTEL_SERVICE_NAME);
+    tracing.AddOtlpExporter(otlpOptions =>
+     {
+         otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+     });
 });
 
 var app = builder.Build();
